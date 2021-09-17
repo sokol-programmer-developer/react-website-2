1b19bcd9ec234b47a716f0336780f6dbfdb3787b
okol-programmer-developer <anvar.safaev@yandex.ru>
2021-09-17 14:11:21 +0300

    create work commit commit-2 gitignore idea clean project clean project-2 commit dev Navbar install react icons install react-router-dom full react-website-2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,42 +2,71 @@ import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
+import * as MdIcons from "react-icons/md"
 
 import {SidebarData} from "./SidebarData";
+import {NavbarData} from "./NavbarData";
 
 import './Navbar.css';
 
 const Navbar = () => {
+
+
     const [sidebar, setSidebar] = useState(false);
     const showSidebar = () => setSidebar(!sidebar)
     const closeMobileMenu = () => setSidebar(false);
     return (
         <>
             <div className="navbar">
-                <Link to="#" className="menu-bars">
-                    <FaIcons.FaBars onClick={showSidebar}/>
-                </Link>
-            </div>
-            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-                <ul className="nav-menu-items">
-                    <li className="navbar-toggle">
-                        <Link to="#" className="menu-bars">
-                            <AiIcons.AiOutlineClose onClick={showSidebar}/>
-                        </Link>
-                    </li>
-                    {SidebarData.map(({id, cName, path, icon, title}) => {
-                        return (
-                            <li key={id} className={cName}>
-                                <Link to={path} onClick={closeMobileMenu}>
-                                    {icon}
-                                    <span>{title}</span>
+                <div className="navbar-desktop-states">
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        <MdIcons.MdFingerprint className='navbar-default-icon' />
+                        <h2 className="navbar-logo-h2">Logo</h2>
+                    </Link>
+
+
+                    <ul className="nav-default-item">
+                        {NavbarData.map(({id, cName, path, icon, title}) => {
+                            return (
+                                <li key={id} className={cName}>
+                                    <Link to={path} onClick={closeMobileMenu}>
+                                        {icon}
+                                        <span>{title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="navbar-mobile-states">
+                    <Link to="#" className="menu-bars">
+                        <FaIcons.FaBars onClick={showSidebar}/>
+                    </Link>
+
+
+                    <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+
+                        <ul className="nav-menu-items">
+                            <li className="navbar-toggle">
+                                <Link to="#" className="menu-bars">
+                                    <AiIcons.AiOutlineClose onClick={showSidebar}/>
                                 </Link>
                             </li>
-                        )
-                    })}
-                </ul>
+                            {SidebarData.map(({id, cName, path, icon, title}) => {
+                                return (
+                                    <li key={id} className={cName}>
+                                        <Link to={path} onClick={closeMobileMenu}>
+                                            {icon}
+                                            <span>{title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
 
-            </nav>
+                    </nav>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/navbar/NavbarData.js
+++ src/components/navbar/NavbarData.js
@@ -1,39 +1,37 @@
-import React from "react";
-
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 import * as FaIcons from "react-icons/fa";
+import React from "react";
 
 
-
-export const SidebarData = [
+export const NavbarData = [
     {
-        id: 5,
+        id: 1,
         title: "Home",
         path: "/",
         icon: <AiIcons.AiFillHome/>,
         cName: "nav-text",
     },
     {
-        id: 6,
+        id: 2,
         title: "Services",
         path: "/services",
         icon: <IoIcons.IoIosPaper/>,
         cName: "nav-text",
     },
     {
-        id: 7,
+        id: 3,
         title: "Products",
         path: "/products",
         icon: <FaIcons.FaCartPlus/>,
         cName: "nav-text",
     },
+
     {
-        id: 8,
+        id: 4,
         title: "Sign_Up",
         path: "/sign-up",
         icon: <FaIcons.FaCartPlus/>,
         cName: "nav-text",
     },
-
 ]
